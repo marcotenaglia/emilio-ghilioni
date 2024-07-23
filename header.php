aebab6f265a86cf519d0c194cd63f07f334d28c7
Marco <marcotenaglia@gmail.com>
2024-07-23 15:45:51 -0300

    commit

--- header.php
+++ header.php
@@ -76,20 +76,32 @@
                         d="M 21 3 C 11.6 3 4 10.6 4 20 C 4 29.4 11.6 37 21 37 C 24.354553 37 27.47104 36.01984 30.103516 34.347656 L 42.378906 46.621094 L 46.621094 42.378906 L 34.523438 30.279297 C 36.695733 27.423994 38 23.870646 38 20 C 38 10.6 30.4 3 21 3 z M 21 7 C 28.2 7 34 12.8 34 20 C 34 27.2 28.2 33 21 33 C 13.8 33 8 27.2 8 20 C 8 12.8 13.8 7 21 7 z">
                     </path>
                 </svg>
-                <svg id="xSvg" class="x" role="img" width="24" height="24" viewBox="0 0 24 24" aria-hidden="false"
-                    aria-label="close" class="BaseIcon__Svg-sc-ov0uhu-0 fylveX">
-                    <path fill="#141824" fill-rule="evenodd"
-                        d="M20.646 5.452a.5.5 0 000-.707l-1.391-1.391a.5.5 0 00-.707 0L12 9.9 5.452 3.354a.5.5 0 00-.707 0L3.354 4.745a.5.5 0 000 .707L9.9 12l-6.547 6.548a.5.5 0 000 .707l1.391 1.391a.5.5 0 00.707 0L12 14.1l6.548 6.547a.5.5 0 00.707 0l1.392-1.391a.5.5 0 000-.707L14.098 12l6.547-6.548z"
-                        clip-rule="evenodd"></path>
-                </svg>
+                
+            
+        <svg id="xSvg" class="x" xmlns="http://www.w3.org/2000/svg" id="Calque_1" data-name="Calque 1"
+            viewBox="0 0 20 20">
+            <defs>
+                <style>
+                .cls-1 {
+                    fill: none;
+                    stroke: #000;
+                    stroke-miterlimit: 10;
+                    stroke-width: 0.75px;
+                }
+                </style>
+            </defs>
+            <title>Plan de travail 1</title>
+            <line class="cls-1" x1="0.5" y1="1.5" x2="19.5" y2="18.5" />
+            <line class="cls-1" x1="0.5" y1="18.5" x2="19.5" y2="1.5" />
+        </svg>
+                
             </div>
         </div>
     </div>
 
 
-    <?php /*esto no existe */ ?>
-    <div class="nav-container nav-list-close">
-        <div class="nav-list-close" id="nav-list">
+    
+        <div class="nav-container nav-list-close" id="nav-list">
             <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Home' ) ) ); ?>">inicio</a>
             <a class="nav-anchortag"
                 href="<?php echo esc_url( get_permalink( get_page_by_title( 'Trayectoria' ) ) ); ?>">trayectoria</a>
@@ -98,6 +110,6 @@
             <a class="nav-anchortag"
                 href="<?php echo esc_url( get_permalink( get_page_by_title( 'Contacto' ) ) ); ?>">contacto</a>
         </div>
-    </div>
+
 
 </header>
